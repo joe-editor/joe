a53b034e913c7216cb80de34558f0040a44a9786
Joe Allen <jhallenworld@gmail.com>
2015-11-17 18:38:35 -0500

    Remove dependency on EngNotation.c (since it requires C99).

--- joe/umath.c
+++ joe/umath.c
@@ -6,7 +6,6 @@
  *	This file is part of JOE (Joe's Own Editor)
  */
 #include "types.h"
-#include "EngNotation.h"
 
 const char *merr;
 
@@ -962,10 +961,189 @@ static void insert_commas(char *dst, char *src)
 	*dst = 0;
 }
 
+/* Convert floating point ascii number into engineering format */
+
+static char *eng(char *d, size_t d_len, const char *s)
+{
+	const char *s_org = s;
+	char *d_org = d;
+	char a[128];
+	int a_len;
+	int sign;
+	int dp;
+	int exp;
+	int exp_sign;
+	int x;
+	int flg = 0;
+
+	--d_len; /* For terminator */
+
+	/* Get sign of number */
+	if (*s == '-') {
+		sign = 1;
+		++s;
+	} else if (*s == '+') {
+		sign = 0;
+		++s;
+	} else {
+		sign = 0;
+	}
+
+	/* Read digits before decimal point */
+	/* Skip leading zeros */
+	while (*s == '0') {
+		flg = 1;
+		++s;
+	}
+	a_len = 0;
+	while (*s >= '0' && *s <= '9') {
+		a[a_len++] = *s++;
+		flg = 1;
+	}
+	/* Decimal point? */
+	dp = 0;
+	if (*s == '.') {
+		flg = 1;
+		++s;
+		/* If we don't have any digits yet, trim leading zeros */
+		if (!a_len) {
+			while (*s == '0') {
+				++s;
+				++dp;
+			}
+		}
+		if (*s >= '1' && *s <= '9') {
+			while (*s >= '0' && *s <= '9') {
+				a[a_len++] = *s++;
+				++dp;
+			}
+		} else {
+			/* No non-zero digits at all? */
+			dp  = 0;
+		}
+	}
+	/* Trim trailing zeros */
+	while (a_len && a[a_len - 1] == '0') {
+		--a_len;
+		--dp;
+	}
+
+	/* Exponent? */
+	if (*s == 'e' || *s == 'E') {
+		++s;
+		if (*s == '-') {
+			++s;
+			exp_sign = 1;
+		} else if (*s == '+') {
+			++s;
+			exp_sign = 0;
+		} else {
+			exp_sign = 0;
+		}
+		exp = 0;
+		while  (*s >= '0' && *s <= '9') {
+			exp = exp * 10 + *s++ - '0';
+		}
+	} else {
+		exp = 0;
+		exp_sign = 0;
+	}
+
+	/* s should be at end of number now */
+	if (!flg) { /* No digits found? */
+		zlcpy(d, d_len, s_org);
+		return d;
+	}
+
+	/* Sign of exponent */
+	if (exp_sign)
+		exp = -exp;
+	/* Account of position of decimal point in exponent */
+	exp -= dp;
+
+	a[a_len] = 0;
+
+	/* For engineering format, make expoenent a multiple of 3 such that
+	   we have 1 - 3 leading digits */
+
+	/* Don't assume modulus of negative number works consistently */
+	if (exp < 0) {
+		switch((-exp) % 3) {
+			case 0: x = 0; break;
+			case 1: x = 2; break;
+			case 2: x = 1; break;
+		}
+	} else {
+		x = (exp % 3);
+	}
+
+	/* Make exponent a multiple of 3 */
+	exp -= x;
+
+	/* Add zeros to mantissa to account for this */
+	while (x--) {
+		a[a_len++] = '0';
+	}
+
+	/* If number has no digits, add one now */
+	if (!a_len)
+		a[a_len++] = '0';
+
+	/* Position decimal point near the left */
+	dp = (a_len - 1) / 3;
+	dp *= 3;
+
+	/* Adjust exponent for this */
+	exp += dp;
+
+	/* Now print */
+	if (sign && d_len) {
+		*d++ = '-';
+		--d_len;
+	}
+
+	/* Digits to left of decimal point */
+	for (x = 0; x != a_len - dp; ++x) {
+		if (d_len) {
+			*d++ = a[x];
+			--d_len;
+		}
+	}
+
+	/* Any more digits? */
+	if (dp) {
+		if (d_len) {
+			*d++ = '.';
+			--d_len;
+		}
+		for (x = a_len - dp; x != a_len; ++x) {
+			if (d_len) {
+				*d++ = a[x];
+				--d_len;
+			}
+		}
+		/* Trim trailing zeros */
+		while (d != d_org && d[-1] == '0') {
+			--d;
+			++d_len;
+		}
+	}
+
+	/* Exponent? */
+	if (exp) {
+		joe_snprintf_1(d, d_len + 1, "e%d", exp);
+	} else {
+		*d = 0;
+	}
+
+	return d_org;
+}
+
 /* Main user interface */
 static int domath(W *w, char *s, void *object, int *notify, int secure)
 {
 	char buf[128];
+	char buf1[128];
 	double result;
 	BW *bw;
 	WIND_BW(bw, w);
@@ -985,7 +1163,8 @@ static int domath(W *w, char *s, void *object, int *notify, int secure)
 			insert_commas(msgbuf, buf);
 			break;
 		} case 1: { /* Engineering */
-			to_engineering_string(buf, SIZEOF(buf), result, 6, 1);
+			joe_snprintf_1(buf1, SIZEOF(buf1), "%.16G", result);
+			eng(buf, sizeof(buf), buf1);
 			insert_commas(msgbuf, buf);
 			break;
 		} case 2: { /* Hex */
