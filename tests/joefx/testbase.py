5257f817eb1ba5cb4834d9c881ed30e6c0653313
John J. Jordan <jj@jjjordan.io>
2017-04-11 10:09:55 +0000

    Fix broken test

--- tests/joefx/testbase.py
+++ tests/joefx/testbase.py
@@ -231,6 +231,7 @@ class JoeTestBase(unittest.TestCase):
         self.assertTextAt("Menu:", x=0)
         self.write(menuname)
         self.rtn()
+        
         # Wait for cursor to be on inverse (selected) text
         self.assertTrue(self.joe.expect(lambda: len(self._readSelected(self.joe.cursor.X, self.joe.cursor.Y)) > 0))
     
@@ -250,9 +251,20 @@ class JoeTestBase(unittest.TestCase):
             self.write(filename)
         self.rtn()
     
-    def selectMenu(self, label):
+    def selectMenu(self, label, menuname='root'):
         """Selects a menu that matches the label (as specified in joerc) for that menu's text"""
-        # Need to detect when we're at the first entry
+        
+        # If we're already on the item, then return it.
+        cursor = self.joe.cursor
+        if utils.compareMenuLabel(label, self._readSelected(cursor.X, cursor.Y)):
+            return True
+        
+        # Jump to first menu item.
+        self.cmd("bofmenu", scope="menu")
+        menu = self.config.getMenu(menuname)
+        self.joe.expect(lambda: utils.compareMenuLabel(menu.items[0].label, self._readSelected(self.joe.cursor.X, self.joe.cursor.Y)))
+        
+        # Move right until we find the item.
         while True:
             cursor = self.joe.cursor
             self.joe.flushin()
