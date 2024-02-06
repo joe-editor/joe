d480e4dee8f90890ece38f2bda67bb3ebf957ece
John J. Jordan <jj@jjjordan.io>
2024-02-05 20:25:59 -0800

    SF Patch 122: delimiter stack by Charles J. Tabony

--- joe/syntax.h
+++ joe/syntax.h
@@ -35,6 +35,10 @@ struct high_cmd {
 	unsigned stop_buffering : 1;	/* Set if we should stop buffering */
 	unsigned save_c : 1;		/* Save character */
 	unsigned save_s : 1;		/* Save string */
+	unsigned push_c : 1;		/* Push character */
+	unsigned push_s : 1;		/* Push string */
+	unsigned pop_c : 1;		/* Push character */
+	unsigned pop_s : 1;		/* Push string */
 	unsigned ignore : 1;		/* Set to ignore case */
 	unsigned start_mark : 1;	/* Set to begin marked area including this char */
 	unsigned stop_mark : 1;		/* Set to end marked area excluding this char */
@@ -58,20 +62,30 @@ struct high_frame {
 	struct high_state *return_state;	/* Return state in the caller's subroutine */
 };
 
+/* delimiter stack frame */
+
+struct high_delim_frame {
+	struct high_delim_frame *parent;	/* Previous pushed delimiter buffer */
+	struct high_delim_frame *child;		/* Next pushed delimiter buffer */
+	struct high_delim_frame *sibling;	/* Another pushed delimiter buffer at the same level */
+	const int *saved_s;			/* Pushed delimiter buffer */
+};
+
 /* Loaded form of syntax file or subroutine */
 
 struct high_syntax {
-	struct high_syntax *next;	/* Linked list of loaded syntaxes */
-	char *name;			/* Name of this syntax */
-	char *subr;			/* Name of the subroutine (or NULL for whole file) */
-	struct high_param *params;	/* Parameters defined */
-	struct high_state **states;	/* The states of this syntax.  states[0] is idle state */
-	HASH *ht_states;		/* Hash table of states */
-	ptrdiff_t nstates;		/* No. states */
-	ptrdiff_t szstates;		/* Malloc size of states array */
-	struct color_def *color;	/* Linked list of color definitions */
-	struct high_cmd default_cmd;	/* Default transition for new states */
-	struct high_frame *stack_base;  /* Root of run-time call tree */
+	struct high_syntax *next;			/* Linked list of loaded syntaxes */
+	char *name;					/* Name of this syntax */
+	char *subr;					/* Name of the subroutine (or NULL for whole file) */
+	struct high_param *params;			/* Parameters defined */
+	struct high_state **states;			/* The states of this syntax.  states[0] is idle state */
+	HASH *ht_states;				/* Hash table of states */
+	ptrdiff_t nstates;				/* No. states */
+	ptrdiff_t szstates;				/* Malloc size of states array */
+	struct color_def *color;			/* Linked list of color definitions */
+	struct high_cmd default_cmd;			/* Default transition for new states */
+	struct high_frame *stack_base;			/* Root of run-time call tree */
+	struct high_delim_frame *delim_stack_base;	/* Root of run-time delimiter buffer tree */
 };
 
 /* Find a syntax.  Load it if necessary. */
@@ -83,10 +97,10 @@ struct high_syntax *load_syntax(const char *name);
 HIGHLIGHT_STATE parse(struct high_syntax *syntax,P *line,HIGHLIGHT_STATE state,struct charmap *charmap);
 extern int *attr_buf;
 
-#define clear_state(s) (((s)->saved_s = 0), ((s)->state = 0), ((s)->stack = 0))
-#define invalidate_state(s) (((s)->state = -1), ((s)->saved_s = 0), ((s)->stack = 0))
+#define clear_state(s) (((s)->saved_s = 0), ((s)->state = 0), ((s)->stack = 0), ((s)->delim_stack = 0))
+#define invalidate_state(s) (((s)->state = -1), ((s)->saved_s = 0), ((s)->stack = 0), ((s)->delim_stack = 0))
 #define move_state(to,from) (*(to)= *(from))
-#define eq_state(x,y) ((x)->state == (y)->state && (x)->stack == (y)->stack && (x)->saved_s == (y)->saved_s)
+#define eq_state(x,y) ((x)->state == (y)->state && (x)->stack == (y)->stack && (x)->delim_stack == (y)->delim_stack && (x)->saved_s == (y)->saved_s)
 
 extern struct high_syntax *syntax_list;
 
